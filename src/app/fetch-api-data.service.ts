6b184a445cc4343f621ff44076ba986934458f91
Mcelest19 <mrn.celestino@gmail.com>
2023-06-08 14:03:38 +0200

    refactor: changing code according new components

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -8,6 +8,10 @@ import { Observable, throwError } from 'rxjs';
 const apiUrl = 'https://movie-api-gas8.onrender.com/';
 @Injectable({ providedIn: 'root'})
 
+@Injectable({
+  providedIn: 'root'
+})
+
 export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
@@ -87,24 +91,26 @@ getOneGenre(genreName: string): Observable<any> {
 
 // Making the api call for the get one user endpoint
 getOneUser(): Observable<any> {
-  const username = localStorage.getItem('username');
-  const token = localStorage.getItem('token');
-  return this.http.get(apiUrl + 'users/' + username, {
-    headers: new HttpHeaders(
-      {
-        Authorization: 'Bearer ' + token,
-      })
-  }).pipe(
-    map(this.extractResponseData),
-    catchError(this.handleError)
-  );
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  return user;
+  // const username = localStorage.getItem('username');
+  // const token = localStorage.getItem('token');
+  // return this.http.get(apiUrl + 'users/' + username, {
+  //   headers: new HttpHeaders(
+  //     {
+  //       Authorization: 'Bearer ' + token,
+  //     })
+  // }).pipe(
+  //   map(this.extractResponseData),
+  //   catchError(this.handleError)
+  // );
 }
 
 // Making the api call for the get favourite movies for a user endpoint
 getFavoriteMovies(): Observable<any> {
-  const username = localStorage.getItem('username');
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
   const token = localStorage.getItem('token');
-  return this.http.get(apiUrl + 'users/' + username, {
+  return this.http.get(apiUrl + 'users/' + user.UserName, {
     headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
@@ -118,24 +124,32 @@ getFavoriteMovies(): Observable<any> {
 
 // Making the api call for the add a movie to favourite Movies endpoint
 addFavoriteMovie(movieId: string): Observable<any> {
-  const username = localStorage.getItem('username');
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
   const token = localStorage.getItem('token');
-  return this.http.post(apiUrl + 'users/' + username + '/movies/' + movieId, {
+  user.FavoriteMovies.push(movieId);
+  localStorage.setItem('user', JSON.stringify(user));
+  return this.http.post(apiUrl + 'users/' + user.UserName + '/movies/' + movieId, {}, {
     headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })
+      }),
+    responseType: "text"
   }).pipe(
     map(this.extractResponseData),
     catchError(this.handleError)
   );
 }
 
+isFavoriteMovie(movieId: string): boolean {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  return user.FavoriteMovies.indexOf(movieId) >= 0;
+}
+
 // Making the api call for the edit user endpoint
 editUser(updatedUser: any): Observable<any> {
-  const username = localStorage.getItem('username');
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
   const token = localStorage.getItem('token');
-  return this.http.put(apiUrl + 'users/' + username, updatedUser, {
+  return this.http.put(apiUrl + 'users/' + user.UserName, updatedUser, {
     headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
@@ -148,28 +162,35 @@ editUser(updatedUser: any): Observable<any> {
 
 // Making the api call for the delete user endpoint
 deleteUser(): Observable<any> {
-  const userid = localStorage.getItem('userid');
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
   const token = localStorage.getItem('token');
-  return this.http.delete(apiUrl + 'users/' + userid, {
+  return this.http.delete(apiUrl + 'users/' + user._id, {
     headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
       })
   }).pipe(
-    map(this.extractResponseData),
     catchError(this.handleError)
   );
 }
 
 // Making the api call for deleting a movie from the favorite movies endpoint
 deleteFavoriteMovie(movieId: string): Observable<any> {
-  const username = localStorage.getItem('username');
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
   const token = localStorage.getItem('token');
-  return this.http.delete(apiUrl + 'users/' + username + '/movies/' + movieId, {
+
+  const index = user.FavoriteMovies.indexOf(movieId);
+  console.log(index);
+  if (index > -1) { // only splice array when item is found
+    user.FavoriteMovies.splice(index, 1); // 2nd parameter means remove one item only
+  }
+  localStorage.setItem('user', JSON.stringify(user));
+  return this.http.delete(apiUrl + 'users/' + user.UserName + '/movies/' + movieId, {
     headers: new HttpHeaders(
       {
         Authorization: 'Bearer ' + token,
-      })
+      }),
+    responseType: "text"
   }).pipe(
     map(this.extractResponseData),
     catchError(this.handleError)
@@ -182,11 +203,14 @@ private extractResponseData(res: any): any {
   return body || {};
 }
 
-
 private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
     console.error('Some error occurred:', error.error.message);
-    } else {
+    } 
+     else if (error.error.errors) {
+      return throwError(() => new Error(error.error.errors[0].msg));
+    }
+    else {
     console.error(
         `Error Status code ${error.status}, ` +
         `Error body is: ${error.error}`);
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
+import { MatDialog } from '@angular/material/dialog';
+import { GenreViewComponent } from '../genre-view/genre-view.component';
+import { DirectorViewComponent } from '../director-view/director-view.component';
+import { SynopsisViewComponent } from '../synopsis-view/synopsis-view.component';
 
+
+// This import is used to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,17 +16,72 @@ import { FetchApiDataService } from '../fetch-api-data.service'
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  favorites: any[] = [];
+  constructor(public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog) { }
 
-ngOnInit(): void {
-  this.getMovies();
-}
+  ngOnInit(): void {
+    this.getMovies();    
+  }
 
-getMovies(): void {
-  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  openGenre(name: string, description: string): void {
+    this.dialog.open(GenreViewComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      width: '280px'
+    });
+  }
+
+  openDirector(name: string, bio: string, birthday: string): void {
+    this.dialog.open(DirectorViewComponent, {
+      data: {
+        Name: name,
+        Bio: bio,
+        Birth: birthday,
+      },
+      //width: '280px'
+    });
+  }
+
+  openSynopsis(title: string, description: string): void {
+    this.dialog.open(SynopsisViewComponent, {
+      data: {
+        Title: title,
+        Description: description,
+      },
+      //width: '280px'
+    });
+  }
+
+  addFavorite(id: string): void {
+    this.fetchApiData.addFavoriteMovie(id).subscribe((result) => {
+
+      this.snackBar.open('Movie added to favorites.', 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  isFavorite(id: string): boolean {
+    return this.fetchApiData.isFavoriteMovie(id);
+  }
+
+  removeFavorite(id: string): void {
+    this.fetchApiData.deleteFavoriteMovie(id).subscribe((result) => {
+      this.snackBar.open('Movie removed from favorites.', 'OK', {
+        duration: 2000
+      });
     });
   }
 }
