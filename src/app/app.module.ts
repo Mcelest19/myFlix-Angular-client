276ceb3f83af8d0d8bb1259d3d6ed318c6833b5e
Mcelest19 <mrn.celestino@gmail.com>
2023-06-08 14:02:16 +0200

    feat: added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { GenreViewComponent } from './genre-view/genre-view.component';
+import { DirectorViewComponent } from './director-view/director-view.component';
+import { SynopsisViewComponent } from './synopsis-view/synopsis-view.component';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -16,22 +23,33 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatListModule } from '@angular/material/list';
+
+
+
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: UserProfileComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserRegistrationFormComponent,
-    UserLoginFormComponent, 
-    UserLoginFormComponent, MovieCardComponent, WelcomePageComponent   
+    UserRegistrationFormComponent,    
+    UserLoginFormComponent,
+    MovieCardComponent, 
+    WelcomePageComponent,      
+    TopBarComponent, 
+    UserProfileComponent,
+    DirectorViewComponent, 
+    GenreViewComponent, 
+    SynopsisViewComponent   
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
@@ -46,7 +64,10 @@ const appRoutes: Routes = [
     MatDialogModule,
     MatSnackBarModule,
     FormsModule,
-    MatIconModule 
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTooltipModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
