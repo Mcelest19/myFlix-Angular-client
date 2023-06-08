2bd9d29b76846bdc85d3cd18ac095260ba7997f0
Mcelest19 <mrn.celestino@gmail.com>
2023-06-08 14:01:12 +0200

    feat: added genre-view component

---
+++ src/app/genre-view/genre-view.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+/**
+ * Component for displaying genre view.
+ */
+@Component({
+  selector: 'app-genre-view',
+  templateUrl: './genre-view.component.html',
+  styleUrls: ['./genre-view.component.css']
+})
+export class GenreViewComponent {
+  
+   /**
+   * @param {Object} data - An object that contains genre details.
+   * @param {string} data.Name - The name of the genre.
+   * @param {string} data.Description - The description of the genre.
+   */
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: String,
+      Description: String,
+    }
+  ) {}
+}
